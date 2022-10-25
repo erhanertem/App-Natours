dd130b67d3b217d05b82272797749303787988f9
Erhan Ertem <eertem@gmail.com>
2022-10-25 12:06:08 +0300

    Refactor CRUD operations based on routes

--- app.js
+++ app.js
@@ -25,42 +25,15 @@ const tours = JSON.parse(
 );
 // console.log(tours); //array
 
-//--->ROUTE HANDLER FOR GET REQUESTS - RECEIVE TOURS INFO
-app.get('/api/v1/tours', (req, res) => {
+//--> ROUTE HANDLER CALLBACKS - CRUD
+const getAllTours = (req, res) => {
   res
     .status(200)
     .json({ status: 'success', results: tours.length, data: { tours } }); //Upon success response, restructure a new response JSON file with status and data itself received earlier
   // res.status(200).json({ status: 'success', data: { tours: tours } }); //same as above
-}); //Its a good practice to specify your API version
+};
 
-//--->ROUTE HANDLER FOR POST REQUESTS - CREATES NEW TOURS
-//IMPORTANT: ON POST ROUTES DATA SEND BY THE CLIENT SHOULD BE INCLUDED IN THE REQ. HOWEVER, EXPRESS.JS DO NOT SUPPORT DATA IN REQ. THEREFORE, WE WOULD NEED MIDDLEWARE TO HANDLE THIS.
-app.post('/api/v1/tours', (req, res) => {
-  // console.log(req.body);
-  // res.send('DONE'); //NOTE: IN ORDER TO COMPLETE POST REQUEST ONE NEED REQ AND RES HANDLED TO COMPLETE THE CYCLE.
-
-  //Get the id of the last element in the dataset
-  const newId = tours[tours.length - 1].id + 1;
-  //Create a new object from an existing object
-  const newTour = Object.assign({ id: newId }, req.body); // merges two objects together
-  //Mutate the read tour database content and add to array this new tour object before we persist it physically to the database file
-  tours.push(newTour);
-
-  //JSON.stringfy is used to change the array to a JSON file before we write/persist this to JSON file.
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    err => {
-      //SUCCESS RESPONSE
-      res.status(201).json({ status: 'success', data: { tour: newTour } }); //code 201 means created - express.json() middleware is required for post requests to work
-    }
-  );
-});
-
-//--->ROUTE HANDLER FOR CUSTOM GET REQUESTS - RECEIVE A SPECIFIC TOUR INFO
-//NOTE: WE CREATE A VARIABLE CALLED id BY PUTTING A COLUMN <:> AFTER SLASH
-// app.get('/api/v1/tours/:id/:trial/:sample?', (req, res) => { //VERY IMPORTANT We can create multiple variables one after another. ? is used to mark it optional so it is upto the client to use it or not...
-app.get('/api/v1/tours/:id', (req, res) => {
+const getTour = (req, res) => {
   // console.log(req);
   console.log(req.params); //returns { id: '5' }
 
@@ -81,10 +54,31 @@ app.get('/api/v1/tours/:id', (req, res) => {
       tour,
     },
   });
-});
+};
 
-//--->ROUTE HANDLER FOR PATCH REQUESTS - AMEND THE SPECIFIC PART OF THE DATA
-app.patch('/api/v1/tours/:id', (req, res) => {
+const createTour = (req, res) => {
+  // console.log(req.body);
+  // res.send('DONE'); //NOTE: IN ORDER TO COMPLETE POST REQUEST ONE NEED REQ AND RES HANDLED TO COMPLETE THE CYCLE.
+
+  //Get the id of the last element in the dataset
+  const newId = tours[tours.length - 1].id + 1;
+  //Create a new object from an existing object
+  const newTour = Object.assign({ id: newId }, req.body); // merges two objects together
+  //Mutate the read tour database content and add to array this new tour object before we persist it physically to the database file
+  tours.push(newTour);
+
+  //JSON.stringfy is used to change the array to a JSON file before we write/persist this to JSON file.
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    err => {
+      //SUCCESS RESPONSE
+      res.status(201).json({ status: 'success', data: { tour: newTour } }); //code 201 means created - express.json() middleware is required for post requests to work
+    }
+  );
+};
+
+const updateTour = (req, res) => {
   console.log(req.params); //returns { id: '5' }
   const id = +req.params.id; //Take the id value inside the req.params object and turn into a number from string
   // console.log(typeof id);
@@ -100,10 +94,9 @@ app.patch('/api/v1/tours/:id', (req, res) => {
     status: 'success',
     data: { tour: '<Updated tour here...>' },
   });
-});
+};
 
-//--->ROUTE HANDLER FOR DELETE REQUESTS
-app.delete('/api/v1/tours/:id', (req, res) => {
+const deleteTour = (req, res) => {
   // console.log(req.params); //returns { id: '5' }
   const id = +req.params.id; //Take the id value inside the req.params object and turn into a number from string
   // console.log(typeof id);
@@ -119,7 +112,30 @@ app.delete('/api/v1/tours/:id', (req, res) => {
     status: 'success',
     data: null,
   });
-});
+};
+
+// //--->ROUTE HANDLER FOR GET REQUESTS - RECEIVE TOURS INFO
+// app.get('/api/v1/tours', getAllTours); //Its a good practice to specify your API version
+// //--->ROUTE HANDLER FOR POST REQUESTS - CREATES NEW TOURS
+// //IMPORTANT: ON POST ROUTES DATA SEND BY THE CLIENT SHOULD BE INCLUDED IN THE REQ. HOWEVER, EXPRESS.JS DO NOT SUPPORT DATA IN REQ. THEREFORE, WE WOULD NEED MIDDLEWARE TO HANDLE THIS.
+// app.post('/api/v1/tours', createTour);
+//---> REFACTORED ROUTE HANDLERS IN ONE-GO
+app.route('/api/v1/tours').get(getAllTours).post(createTour);
+
+// //--->ROUTE HANDLER FOR CUSTOM GET REQUESTS - RECEIVE A SPECIFIC TOUR INFO
+// //NOTE: WE CREATE A VARIABLE CALLED id BY PUTTING A COLUMN <:> AFTER SLASH
+// // app.get('/api/v1/tours/:id/:trial/:sample?', (req, res) => { //VERY IMPORTANT We can create multiple variables one after another. ? is used to mark it optional so it is upto the client to use it or not...
+// app.get('/api/v1/tours/:id', getTour);
+// //--->ROUTE HANDLER FOR PATCH REQUESTS - AMEND THE SPECIFIC PART OF THE DATA
+// app.patch('/api/v1/tours/:id', updateTour);
+// //--->ROUTE HANDLER FOR DELETE REQUESTS
+// app.delete('/api/v1/tours/:id', deleteTour);
+//---> REFACTORED ROUTE HANDLERS IN ONE-GO
+app
+  .route('/api/v1/tours/:id')
+  .get(getTour)
+  .patch(updateTour)
+  .delete(deleteTour);
 
 const port = 3000; //Declare port
 app.listen(port, () => {
