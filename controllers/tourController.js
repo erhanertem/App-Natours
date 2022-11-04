b42222bd02b8a636c385efd8949c61fea04f6a87
Erhan Ertem <eertem@gmail.com>
2022-11-04 16:13:01 +0300

    Gatherup all tourcontroller API  in a class - Refactoring code

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -13,77 +13,134 @@ exports.aliasTopTours = (req, res, next) => {
   next();
 };
 
+class APIFeatures {
+  constructor(query, queryString) {
+    this.query = query; //mongoose model data --> Tour.find()
+    this.queryString = queryString; //i.e. 127.0.0.1:3000/api/v1/tours?fields=name ---> req.query is the {fields: 'name'}
+  }
+
+  filter() {
+    //#1A.Primary off-scope Filtering
+    const queryObj = { ...this.queryString }; //Create a shallow copy of the query object
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach(el => delete queryObj[el]);
+    //#1B.Secondary Advanced Filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+
+    this.query.find(JSON.parse(queryStr));
+    return this; //we return the object to use them in  daisy-chain of these methods
+  }
+
+  sort() {
+    if (this.queryString.sort) {
+      const sortBy = this.queryString.sort.split(',').join(' ');
+      this.query = this.query.sort(sortBy);
+    } else {
+      this.query = this.uery.sort('-createdAt');
+    }
+
+    return this; //we return the object to use them in  daisy-chain of these methods
+  }
+
+  limitFields() {
+    if (this.queryString.fields) {
+      const fields = this.queryString.fields.split(',').join(' ');
+      this.query = this.query.select(fields);
+    } else {
+      this.query = this.query.select('-__v'); //exclude this field - mongoDB by default internally uses this __v field for its operations which is of no use to end-user.
+    }
+    return this; //we return the object to use them in  daisy-chain of these methods
+  }
+
+  paginate() {
+    //127.0.0.1:3000/api/v1/tours?page=2&limit=10 --> page 1 (1-10) , page 2 (11-20), page 3 (21-30)
+    const page = +this.queryString.page || 1; //we stringfy the number input for the page query property or declare default
+    const limit = +this.queryString.limit || 100; //we stringfy the number input for the limit query property or declare default
+    const skip = (page - 1) * limit;
+
+    this.query = this.query.skip(skip).limit(limit);
+
+    return this;
+  }
+}
+
 exports.getAllTours = async (req, res) => {
   try {
     console.log('🎁', req, req.query);
-    //--->#1.BUILD QUERY
-    //#1A.Primary off-scope Filtering
-    const queryObj = { ...req.query }; //Create a shallow copy of the query object
-    const excludedFields = ['page', 'sort', 'limit', 'fields'];
-    excludedFields.forEach(el => delete queryObj[el]);
+    // //--->#1.BUILD QUERY
+    // //#1A.Primary off-scope Filtering
+    // const queryObj = { ...req.query }; //Create a shallow copy of the query object
+    // const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    // excludedFields.forEach(el => delete queryObj[el]);
 
-    //#1B.Secondary Advanced Filtering
-    // { duration: { $gte: '5' }, difficulty: 'easy' }-->mongoDB query
-    // { duration: { gte: '5' }, difficulty: 'easy' } -->queryObj output
-    let queryStr = JSON.stringify(queryObj); //We change JSON obj to a string in order to add $ sign to gte, gt, lte, lt
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`); //b flag matches the excat objects as listed inside the (...|...|...|...) wrapped by g flag which finds multiple copies of the same element(regx global override)
-    // console.log(JSON.parse(queryStr));
+    // //#1B.Secondary Advanced Filtering
+    // // { duration: { $gte: '5' }, difficulty: 'easy' }-->mongoDB query
+    // // { duration: { gte: '5' }, difficulty: 'easy' } -->queryObj output
+    // let queryStr = JSON.stringify(queryObj); //We change JSON obj to a string in order to add $ sign to gte, gt, lte, lt
+    // queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`); //b flag matches the excat objects as listed inside the (...|...|...|...) wrapped by g flag which finds multiple copies of the same element(regx global override)
+    // // console.log(JSON.parse(queryStr));
 
-    //->1ST METHOD OF FILTERING DATA WITH FILTER OBJECT
-    let query = Tour.find(JSON.parse(queryStr)); //mongoose find() method with filter object {}
+    // //->1ST METHOD OF FILTERING DATA WITH FILTER OBJECT
+    // let query = Tour.find(JSON.parse(queryStr)); //mongoose find() method with filter object {}
 
-    //->2ND METHOD OF FILTERING DATA with MONGOOSE QUERY API METHODS
-    // const query = await Tour.find()
-    //   .where('duration')
-    //   .equals(5)
-    //   .where('difficulty')
-    //   .equals('easy'); //mongoose find() method with mongoose chained filter moethods
+    // //->2ND METHOD OF FILTERING DATA with MONGOOSE QUERY API METHODS
+    // // const query = await Tour.find()
+    // //   .where('duration')
+    // //   .equals(5)
+    // //   .where('difficulty')
+    // //   .equals('easy'); //mongoose find() method with mongoose chained filter moethods
 
-    //--->#2.SORTING QUERY/WITH FALLBACK CRITERIA
-    //127.0.0.1:3000/api/v1/tours?sort=price               --> sort by price as fired from postman have re.query return -> { sort: 'price' } - ascending order
-    //127.0.0.1:3000/api/v1/tours?sort=-price               --> sort by price as fired from postman have re.query return -> { sort: 'price' } - descending order
-    //127.0.0.1:3000/api/v1/tours?sort=price,ratingAverage --> sort by price then if its a tie sort by ratingAverage as a fallback sort criteria --> { sort: 'price,ratingAverage' } - ascending order
-    //127.0.0.1:3000/api/v1/tours?sort=price,-ratingAverage --> sort by price then if its a tie sort by ratingAverage as a fallback sort criteria --> { sort: 'price,ratingAverage' } - descending order ratingAvg
-    //127.0.0.1:3000/api/v1/tours?sort=-price,-ratingAverage --> sort by price then if its a tie sort by ratingAverage as a fallback sort criteria --> { sort: 'price,ratingAverage' } - descending order ratingAvg & price
-    if (req.query.sort) {
-      //if request includes sort field ..then....
-      const sortBy = req.query.sort.split(',').join(' ');
-      // console.log(sortBy);
-      query = query.sort(sortBy); //mongoose query sort() method ascending order
-      // query = query.sort('-' + req.query.sort); //mongoose query sort() method descending order
-    } else {
-      // 127.0.0.1:3000/api/v1/tours --> if the user do not specify any sorting criteria sort by createdAt in desc order so that newer ones appears first
-      query = query.sort('-createdAt');
-    }
+    // //--->#2.SORTING QUERY/WITH FALLBACK CRITERIA
+    // //127.0.0.1:3000/api/v1/tours?sort=price               --> sort by price as fired from postman have re.query return -> { sort: 'price' } - ascending order
+    // //127.0.0.1:3000/api/v1/tours?sort=-price               --> sort by price as fired from postman have re.query return -> { sort: 'price' } - descending order
+    // //127.0.0.1:3000/api/v1/tours?sort=price,ratingAverage --> sort by price then if its a tie sort by ratingAverage as a fallback sort criteria --> { sort: 'price,ratingAverage' } - ascending order
+    // //127.0.0.1:3000/api/v1/tours?sort=price,-ratingAverage --> sort by price then if its a tie sort by ratingAverage as a fallback sort criteria --> { sort: 'price,ratingAverage' } - descending order ratingAvg
+    // //127.0.0.1:3000/api/v1/tours?sort=-price,-ratingAverage --> sort by price then if its a tie sort by ratingAverage as a fallback sort criteria --> { sort: 'price,ratingAverage' } - descending order ratingAvg & price
+    // if (req.query.sort) {
+    //   //if request includes sort field ..then....
+    //   const sortBy = req.query.sort.split(',').join(' ');
+    //   // console.log(sortBy);
+    //   query = query.sort(sortBy); //mongoose query sort() method ascending order
+    //   // query = query.sort('-' + req.query.sort); //mongoose query sort() method descending order
+    // } else {
+    //   // 127.0.0.1:3000/api/v1/tours --> if the user do not specify any sorting criteria sort by createdAt in desc order so that newer ones appears first
+    //   query = query.sort('-createdAt');
+    // }
 
-    //--->#3.FIELD LIMITING
-    //127.0.0.1:3000/api/v1/tours?fields=name,duration,difficulty,price --> include only these information fields
-    //127.0.0.1:3000/api/v1/tours?fields=-name,-duration,-difficulty,-price --> exclude only these information fields
+    // //--->#3.FIELD LIMITING
+    // //127.0.0.1:3000/api/v1/tours?fields=name,duration,difficulty,price --> include only these information fields
+    // //127.0.0.1:3000/api/v1/tours?fields=-name,-duration,-difficulty,-price --> exclude only these information fields
 
-    if (req.query.fields) {
-      const fields = req.query.fields.split(',').join(' ');
-      query = query.select(fields);
-    } else {
-      query = query.select('-__v'); //exclude this field - mongoDB by default internally uses this __v field for its operations which is of no use to end-user.
-    }
+    // if (req.query.fields) {
+    //   const fields = req.query.fields.split(',').join(' ');
+    //   query = query.select(fields);
+    // } else {
+    //   query = query.select('-__v'); //exclude this field - mongoDB by default internally uses this __v field for its operations which is of no use to end-user.
+    // }
 
-    //--->#4.PAGINATION
-    //127.0.0.1:3000/api/v1/tours?page=2&limit=10 --> page 1 (1-10) , page 2 (11-20), page 3 (21-30)
+    // //--->#4.PAGINATION
+    // //127.0.0.1:3000/api/v1/tours?page=2&limit=10 --> page 1 (1-10) , page 2 (11-20), page 3 (21-30)
 
-    const page = +req.query.page || 1; //we stringfy the number input for the page query property
-    const limit = +req.query.limit || 100; //we stringfy the number input for the limit query property
-    const skip = (page - 1) * limit;
+    // const page = +req.query.page || 1; //we stringfy the number input for the page query property or declare default
+    // const limit = +req.query.limit || 100; //we stringfy the number input for the limit query property or declare default
+    // const skip = (page - 1) * limit;
 
-    query = query.skip(skip).limit(limit);
+    // query = query.skip(skip).limit(limit);
 
-    //GUARD CLAUSE
-    if (req.query.page) {
-      const numTours = await Tour.countDocuments(); //mongoose model api function that counts all the available docs in a database collection
-      if (skip >= numTours) throw new Error('This page does not exist');
-    }
+    // //GUARD CLAUSE
+    // if (req.query.page) {
+    //   const numTours = await Tour.countDocuments(); //mongoose model api function that counts all the available docs in a database collection
+    //   if (skip >= numTours) throw new Error('This page does not exist');
+    // }
 
     //EXECUTE QUERY
-    const tours = await query;
+    const features = new APIFeatures(Tour, req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate(); //create an instance of APIFeatures class
+    const tours = await features.query;
 
     //SEND SUCCESS RESPONSE
     res.status(200).json({
