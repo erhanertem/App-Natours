fec997fb6acb7634f4008ee976085b702db65e62
Erhan Ertem <eertem@gmail.com>
2022-11-05 13:53:25 +0300

    Add notes for APIFeatures query mongoose object

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -15,7 +15,17 @@ exports.aliasTopTours = (req, res, next) => {
 
 class APIFeatures {
   constructor(query, queryString) {
-    this.query = query; //mongoose model data --> Tour.find()
+    this.query = query; //mongoose model data object --> Tour.find() for daisy chaining queries
+    /* //VERY IMPORTANT
+The Model.find() function returns an instance of Mongoose's Query class. The Query class represents a raw CRUD operation that you may send to MongoDB. It provides a chainable interface for building up more sophisticated queries. You don't instantiate a Query directly, Customer.find() instantiates one for you.
+    */
+    /* //VERY IMPORTANT
+So, the APIFeatures class expects a mongoose query object as an input. The way we create a query object is by creating a query with Tour.find(), but not executing the query right away, so not using await on it (in case we're using async/await like we do in the course). Again, by doing this, we end up with a query object onto which we can then chain other methods, such as sort, or another find, as you posted in your example: this.query.find(JSON.parse(queryStr))
+Keep in mind that here, inside the class, this.query is the query object we created in the beginning, so it's like having: Tour.find().find(JSON.parse(queryStr))
+And yes, that is totally acceptable. Again, because the query has not yet executed, it didn't return the actual results yet. That's what we do in the end, which is the reason why in the end we have to use
+const tours = await features.query;
+   */
+
     this.queryString = queryString; //i.e. 127.0.0.1:3000/api/v1/tours?fields=name ---> req.query is the {fields: 'name'}
   }
 
@@ -37,7 +47,7 @@ class APIFeatures {
       const sortBy = this.queryString.sort.split(',').join(' ');
       this.query = this.query.sort(sortBy);
     } else {
-      this.query = this.uery.sort('-createdAt');
+      this.query = this.query.sort('-createdAt');
     }
 
     return this; //we return the object to use them in  daisy-chain of these methods
@@ -67,7 +77,7 @@ class APIFeatures {
 
 exports.getAllTours = async (req, res) => {
   try {
-    console.log('🎁', req, req.query);
+    // console.log('🎁', req.query);
     // //--->#1.BUILD QUERY
     // //#1A.Primary off-scope Filtering
     // const queryObj = { ...req.query }; //Create a shallow copy of the query object
@@ -135,7 +145,8 @@ exports.getAllTours = async (req, res) => {
     // }
 
     //EXECUTE QUERY
-    const features = new APIFeatures(Tour, req.query)
+    // console.log('🧨', Tour.find(), '🧨');
+    const features = new APIFeatures(Tour.find(), req.query) //(query object, express query string)
       .filter()
       .sort()
       .limitFields()
