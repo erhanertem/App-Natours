ceac03675def097661948f2eb770a6b35e69d00f
Erhan Ertem <eertem@gmail.com>
2022-11-10 12:25:00 +0300

    Create user mongoose database schema & model

---
+++ models/userModel.js
@@ -0,0 +1,33 @@
+//-->IMPORT 3RD PARTY MODULE
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+//->CREATE A BASIC MONGOOSE SCHEMA FOR USERS
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please tell us your name'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide your email'],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Please provide a valid email'],
+  },
+  photo: String,
+  password: {
+    type: String,
+    required: [true, 'Please provide password'],
+    minlength: 8,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Please confirm your password'],
+  },
+});
+
+//->CREATE A MODEL OUT OF THE CREATED SCHEMA FOR A USER
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
