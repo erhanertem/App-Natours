b6dda77393577c8cb74c0b91d6bd97b76b1039e9
Erhan Ertem <eertem@gmail.com>
2022-11-26 12:35:46 +0300

    Add update user function (admin)

--- controllers/userController.js
+++ controllers/userController.js
@@ -87,10 +87,6 @@ exports.getUser = (req, res) => {
     .status(500)
     .json({ status: 'error', message: 'This route is not yet defined' });
 };
-exports.updateUser = (req, res) => {
-  res
-    .status(500)
-    .json({ status: 'error', message: 'This route is not yet defined' });
-};
+exports.updateUser = factory.updateOne(User);
 
 exports.deleteUser = factory.deleteOne(User);
