d8d30c59ee65bd27d8e5f0eeeda92c1a3d6f08c9
Erhan Ertem <eertem@gmail.com>
2022-11-26 10:54:47 +0300

    Add delete user function (admin)

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,7 @@
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
+const factory = require('./handlerFactory');
 
 //-->#2.HELPER FUNCTIONS
 const filterObj = (reqBody, ...allowedFields) => {
@@ -91,8 +92,5 @@ exports.updateUser = (req, res) => {
     .status(500)
     .json({ status: 'error', message: 'This route is not yet defined' });
 };
-exports.deleteUser = (req, res) => {
-  res
-    .status(500)
-    .json({ status: 'error', message: 'This route is not yet defined' });
-};
+
+exports.deleteUser = factory.deleteOne(User);
