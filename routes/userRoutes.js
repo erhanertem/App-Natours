abf9f705f1f2f0f3497b1a6e57d0169f7d9cb6b6
Erhan Ertem <eertem@gmail.com>
2022-11-27 16:57:14 +0300

    Protecting all routes at the same time via router.use() middleware instruction

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -16,28 +16,40 @@ router.post('/login', authController.login);
 router.post('/forgotPassword', authController.forgotPassword);
 router.patch('/resetPassword/:token', authController.resetPassword); //resetpassword with the temp passtoken
 
+router.use(authController.protect); //ONCE WE STARTR USING AUTHCONTROLLER.PROTECT, IT WOULD APPLY TO ANYTHING BELOW...
+
 router.patch(
   '/updateMyPassword',
-  authController.protect,
+  // authController.protect,
   authController.updatePassword
 ); //Update password of logged in user
 
 router.get(
   '/me',
-  authController.protect,
+  // authController.protect,
   userController.getMe,
   userController.getUser
 );
-router.patch('/updateMe', authController.protect, userController.updateMe); //Let the user update his own data
-router.delete('/deleteMe', authController.protect, userController.deleteMe); //Let the user delete himself
+router.patch(
+  '/updateMe',
+  // authController.protect,
+  userController.updateMe
+); //Let the user update his own data
+router.delete(
+  '/deleteMe',
+  // authController.protect,
+  userController.deleteMe
+); //Let the user delete himself
 
 router
+  //THIS ROUTE OPERATIONS ARE PROTECTED BY router.use(authController.protect);
   .route('/') //Instead of repeating the HTTP verb for every single route, we define a common HTTP verb from which multiple HTTP methods do share
   .get(userController.getAllUsers)
   .post(userController.createUser);
 
 router
   .route('/:id')
+  //THIS ROUTE OPERATIONS ARE PROTECTED BY router.use(authController.protect);
   .get(userController.getUser)
   .patch(userController.updateUser)
   .delete(userController.deleteUser);
