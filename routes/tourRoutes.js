40b5fea2af9d70eb453a75cc508d808b187b5053
Erhan Ertem <eertem@gmail.com>
2022-11-27 16:40:34 +0300

    Revisit tourRoutes to make mods to monthly plans available only for admin, lead guides and normal guides

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -43,7 +43,13 @@ router
 
 router.route('/tour-stats').get(tourController.getTourStats);
 
-router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan);
+router
+  .route('/monthly-plan/:year')
+  .get(
+    authController.protect,
+    authController.restrictTo('admin', 'lead-guide', 'guide'),
+    tourController.getMonthlyPlan
+  );
 
 router
   .route('/:id')
