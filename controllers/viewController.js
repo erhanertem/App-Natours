c29ecf80a23251b1b6dce9a80809e38321541e2a
Erhan Ertem <eertem@gmail.com>
2022-12-07 01:14:10 +0300

    Complete overview page pug template

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,6 +1,17 @@
-exports.getOverview = (req, res) => {
-  res.status(200).render('overview', { title: 'All tours' });
-};
+const Tour = require('../models/tourModel');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getOverview = catchAsync(async (req, res) => {
+  //Step#1 Get tour data from collection
+  const tours = await Tour.find();
+  //Step#2 Build template
+
+  //Step#3 Render that template using tour data from Step#1
+  res.status(200).render('overview', {
+    title: 'All tours',
+    tours, // sames as tours: tours, for ES6
+  });
+});
 
 exports.getTour = (req, res) => {
   res.status(200).render('tour', { title: 'The Forest Hiker Tour' });
