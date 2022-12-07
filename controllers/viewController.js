e421ab3c8359add3a1e74c7de1c54b71ec45139f
Erhan Ertem <eertem@gmail.com>
2022-12-07 18:56:56 +0300

    Add PUG mixin and insert tour variables on the tour pug template - Make an external ref to a PUG mixin

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,6 +1,5 @@
 const Tour = require('../models/tourModel');
 const catchAsync = require('../utils/catchAsync');
-const { getAllReviews } = require('./reviewController');
 
 exports.getOverview = catchAsync(async (req, res) => {
   //Step#1 Get tour data from collection
@@ -22,7 +21,7 @@ exports.getTour = catchAsync(async (req, res) => {
   //Step#2 Build template
   //Step#3 Render template using the data from Step#1
   res.status(200).render('tour', {
-    title: 'The Forest Hiker Tour',
+    title: 'The Forest Hiker Tour', //browser tab title
     tour, // same as tour: tour, for ES6
   });
 });
