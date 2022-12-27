91b4f44910e64651ec54579c53368b6782efeeb8
Erhan Ertem <eertem@gmail.com>
2022-12-27 16:22:37 +0300

    Creating mongoose model for bookings

---
+++ models/bookingModel.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const bookingSchema = new mongoose.Schema({
+  tour: {
+    type: mongoose.Schema.ObjectId, // same as type: mongoose.ObjectId,
+    ref: 'Tour',
+    required: [true, 'Booking must beloing to a Tour!'],
+  },
+  user: {
+    type: mongoose.Schema.ObjectId, // same as type: mongoose.ObjectId,
+    ref: 'User',
+    required: [true, 'Booking must belong to a User!'],
+  },
+  price: {
+    type: Number,
+    required: [true, 'Booking must have a price.'],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  paid: {
+    type: Boolean,
+    default: true,
+  },
+});
+
+bookingSchema.pre(/^find/, function (next) {
+  this.populate('user').populate({ path: 'tour', select: 'name' }); //Find the user data, the tour selected and its name pre populated when find***  operation by admin, etc. is conducted...
+}); //Will not often happen, since only admin, guides and tour leaders are allowed to do this....
+
+const Booking = mongoose.model('Booking', bookingSchema);
+
+module.exports = Booking;
