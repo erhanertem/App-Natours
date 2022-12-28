7f9bface6b9160c607ed5460aae4447b13d50b9c
Erhan Ertem <eertem@gmail.com>
2022-12-28 16:05:17 +0300

    Provide entire booking API

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -7,7 +7,7 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const Tour = require('../models/tourModel');
 const Booking = require('../models/bookingModel');
 const catchAsync = require('../utils/catchAsync');
-// const factory = require('./handlerFactory');
+const factory = require('./handlerFactory');
 // const AppError = require('../utils/appError');
 
 exports.getCheckoutSession = catchAsync(async (req, res, next) => {
@@ -67,3 +67,10 @@ exports.createBookingCheckout = catchAsync(async (req, res, next) => {
 
   res.redirect(req.originalUrl.split('?')[0]);
 });
+
+exports.createBooking = factory.createOne(Booking);
+exports.getAllBookings = factory.getAll(Booking);
+
+exports.getBooking = factory.getOne(Booking);
+exports.updateBooking = factory.updateOne(Booking);
+exports.deleteBooking = factory.deleteOne(Booking);
--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -9,6 +9,8 @@ const authController = require('../controllers/authController');
 const router = express.Router();
 
 //-->#3.DEFINE ROUTES
+router.use(authController.protect); //Protect all routes below
+//Accessed by all user types
 // #1.way
 // router
 //   .route('/checkout-session/:tourId')
@@ -16,9 +18,21 @@ const router = express.Router();
 // #2.way
 router.get(
   '/checkout-session/:tourId',
-  authController.protect,
+  // authController.protect,
   bookingController.getCheckoutSession
 );
+
+//Limited access routes
+router.use(authController.restrictTo('admin', 'lead-guide'));
+router
+  .route('/')
+  .get(bookingController.getAllBookings)
+  .post(bookingController.createBooking);
+router
+  .route('/:id')
+  .get(bookingController.getBooking)
+  .patch(bookingController.updateBooking)
+  .delete(bookingController.deleteBooking);
 
 //-->#4.EXPORT MODULE
 module.exports = router;
